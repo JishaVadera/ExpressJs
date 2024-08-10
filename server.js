79a497380ac79c928efad6f7271276a5944e769d
JishaVadera <jishamvadera04@gmail.com>
2024-08-10 18:18:29 +0530

    lecture1

---
+++ server.js
@@ -0,0 +1,16 @@
+// --------------------------lecture1----------------------------------------
+
+// const express = require('express');
+// const server = express();
+
+// server.get("/", (req,res) => {
+//     res.write("Welcome to main server");
+//     res.end();
+// })
+// server.get("/login" , (req,res) => {
+//     res.write("Welcome to login page");
+//     res.end();
+// })
+// server.listen(1409 , () => {
+//     console.log("Server is running on port 1409 at http://localhost:1409");
+// })
