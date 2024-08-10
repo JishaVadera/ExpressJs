999c1ee905395c3e8ea9929a3dcaea3207e04a34
JishaVadera <jishamvadera04@gmail.com>
2024-08-10 18:23:14 +0530

    lecture2

--- server.js
+++ server.js
@@ -1,16 +1,108 @@
-// --------------------------lecture1----------------------------------------
+
+// --------------------------lecture2----------------------------------------
+const express = require('express');
+const server = express();   // create server
+const morgan = require('morgan');
+
+server.use(morgan('dev'));
+
+const loggerFun = (req, res, next) => {
+    console.log(req.ip, req.url, req.method);
+    next();
+}
+server.use(loggerFun);
+
+// in-built middleware
+server.use(express.json());
+server.use(express.urlencoded({ extended: false}));
+server.use("/hello", express.static('public'));
+
+const myFun = (req, res, next) =>{
+    console.log(req.body);
+    next();
+    // if(req.query.age >= 18){
+    //     console.log('Success');
+    //     next();
+    // }else{
+    //     res.Json({message: "Sorry you are not allowed to visit this website ..."})
+    // }
+}
+
+// server.use(myFun);  // application
+
+// POST, GET PUT, PATCH, DELETE
+server.get('/', (req, res) => {
+    res.write('Welcome to Express');
+    res.end();
+})
+
+server.get('/login', myFun, (req, res) => {
+    res.write({msg:'Welcome to Login Page'});
+    res.end();
+})
+
+server.post('/', (req, res) => {
+    // res.write('Welcome to Post Method');
+    res.send('Welcome to Post Method');
+})
+
+
+server.listen(8000, () => {
+    console.log('Server Start at http://localhost8000');
+});
+
+
+// ----------------------------------------part2
+
 
 // const express = require('express');
-// const server = express();
+// const server = express();   // create server
+// const data = require('./friend.json');
+// const fs = require('fs');
+// const data = fs.readFileSync('./friend.json', 'utf8');
+// console.log(data);
 
-// server.get("/", (req,res) => {
-//     res.write("Welcome to main server");
+// const myFun = (req, res, next) =>{
+//     // console.log(req.query);
+//     if(req.query.age >= 18){
+//         console.log('Success');
+//         next();
+//     }else{
+//         res.Json({message: "Sorry you are not allowed to visit this website ..."})
+//     }
+// }
+
+// server.use(myFun);  // application
+
+// POST, GET PUT, PATCH, DELETE
+// server.get('/', (req, res) => {
+//     res.write('Welcome to Express');
 //     res.end();
 // })
-// server.get("/login" , (req,res) => {
-//     res.write("Welcome to login page");
+
+// server.get('/login', myFun, (req, res) => {
+//     res.write({msg:'Welcome to Login Page'});
 //     res.end();
 // })
-// server.listen(1409 , () => {
-//     console.log("Server is running on port 1409 at http://localhost:1409");
+
+// server.post('/', (req, res) => {
+//     // res.write('Welcome to Post Method');
+//     res.send('Welcome to Post Method');
 // })
+
+// server.put('/', (req, res) => {
+//     res.json({msg:'Hello Put Method Called'});
+// })
+
+// server.patch('/', (req, res) => {
+//     res.status(400);
+//     res.json({msg:'Hello Patch Method Called'});
+// })
+
+// server.get('/user', (req, res) => {
+//     res.json(JSON.parse(data));
+// })
+
+// server.listen(8000, () => {
+//     console.log('Server Start at http://localhost:8000');
+// });
